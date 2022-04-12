9d82bd2378ad653e1af07e8df9152040e5074f4e
Danilo Stojkovic <danilostojkovic749@gmail.com>
2022-04-12 11:29:47 +0200

    prenotazione sessione ora vede il giorno della settimana

--- js/caricaDateOre.js
+++ js/caricaDateOre.js
@@ -1,4 +1,5 @@
 var mesi = ["gennaio", "febbraio", "marzo", "aprile", "maggio", "giugno", "luglio", "agosto", "settembre", "ottobre", "novembre", "dicembre"];
+var settimana = ["domenica", "lunedì", "martedì", "mercoledì", "giovedì", "venerdì", "sabato"];
 
 function caricaDateOre(){
     var today = new Date();
@@ -30,19 +31,56 @@ function caricaDateOre(){
         document.getElementById("oraFine").selectedIndex = h-10;
         document.getElementById("minutoFine").selectedIndex = 2;
     }
+
+    var dotw = today.getDay();
+    document.getElementById("giornoSettimana").innerHTML = settimana[dotw];
 }
 
 function meseCambiato(){
     var e = document.getElementById("meseSessione");
     var mm = e.options[e.selectedIndex].value;
-    console.log(mm);
     mm = String(mm).padStart(2, '0');
     var n = numeroDiGiorni(mm, new Date().getFullYear());
-    var htmlCode = "";
-    for(var i = 1; i <= n; i++){
+    var htmlCode = "<option value='1' selected >01</option>";
+    for(var i = 2; i <= n; i++){
         htmlCode += "<option value='" + i + "'>" + String(i).padStart(2, '0') + "</option>";
     }
     document.getElementById("giornoSessione").innerHTML = htmlCode;
+    giornoCambiato();
+}
+
+function giornoCambiato(){
+    var today = new Date();
+    var date = new Date(today.getFullYear(), document.getElementById("meseSessione").value - 1, document.getElementById("giornoSessione").value);
+    
+    var dotw = date.getDay();
+    document.getElementById("giornoSettimana").innerHTML = settimana[dotw];
+    var dalle = 10, alle = 22;
+    var contenuto = "";
+    if(dotw == 6){    //è sabato
+        dalle = 11;
+        alle = 16;
+    }
+    else if(dotw == 0){      //è domenica
+        document.getElementById("selezioneOrari").innerHTML = "<p>Non è possibile prenotare sessioni di domenica</p>";
+        return;
+    }
+    var contenuto = "<div><label for='oraInizio minutoInizio'>Ora d'inizio della sessione:</label><div><select id='oraInizio'>";
+    contenuto += "<option value='" + dalle + "' selected>" + String(dalle) + "</option>";
+    for(i=dalle + 1; i<=alle; i++){
+        contenuto += "<option value='" + i + "'>" + String(i) + "</option>";
+    }
+    contenuto += "</select><select id='minutoInizio'><option value='0' selected>00</option><option value='15'>15</option><option value='30'>30</option><option value='45'>45</option></select>";
+    contenuto += "</div></div><div><label for='oraFine minutoFine'>Ora di fine della sessione:</label><div><select id='oraFine'>";
+    contenuto += "<option value='" + dalle + "' selected>" + String(dalle) + "</option>";
+    for(i=dalle + 1; i<=alle; i++){
+        contenuto += "<option value='" + i + "'>" + String(i) + "</option>";
+    }
+    contenuto += "</select><select id='minutoFine'><option value='0'>00</option><option value='15'>15</option><option value='30' selected>30</option><option value='45'>45</option></select>";
+    contenuto += "</div></div><input type='submit' value='prenota' name='prenota'>";
+
+    document.getElementById("selezioneOrari").innerHTML = contenuto;
+
 }
 
 function numeroDiGiorni(mm, yyyy){
