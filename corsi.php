9234c511df5f4a5c0595735ff9d47cc8dcccea5a
Matteo Midena <matteo.midena@protonmail.com>
2022-05-01 09:17:08 +0000

    Inizio modals per pagina corsi

--- corsi.php
+++ corsi.php
@@ -10,11 +10,29 @@ $modello = new Corso();
 $corsi = $modello->index($_GET);
 
 $htmlCorsi = "";
+$modalCorsi = "";
 if(count($corsi) > 0){
     foreach($corsi as $corso){
         $htmlCorsi .= "<div class='corso-card'>
-            <img class='img-card' src='" . ($corso['copertina']?:'img/spinningQuadrato.jpg') . "'/>
-            <p class='title-card'>" . $corso['titolo'] . "<span class='little-title'><span xml:lang='en'> by</span> " . $corso['trainer_nome'] . "</span></p>
+                <img class='img-card' src='" . ($corso['copertina']?:'img/spinningQuadrato.jpg') . "'/>
+                <p class='title-card'>" . $corso['titolo'] . "<span class='little-title'><span xml:lang='en'> by</span> " . $corso['trainer_nome'] . "</span></p>
+                <button id='btnModalCorso". $corso['id'] . "'> Leggi di più </button> 
+        </div>";
+        /*
+        "<div class='corso-card'>
+            <a href='corso.php?id=" . $corso['id'] . "'>
+                <img class='img-card' src='" . ($corso['copertina']?:'img/spinningQuadrato.jpg') . "'/>
+                <p class='title-card'>" . $corso['titolo'] . "<span class='little-title'><span xml:lang='en'> by</span> " . $corso['trainer_nome'] . "</span></p>
+            </a>
+        </div>";
+        */
+        $modalCorsi .= "<div id='modalCorso". $corso['id'] . "' class='modal'>
+            <!-- Modal content -->
+            <div class='modal-content'>
+                <span class='close' id='close" . $corso['id'] . "'>&times;</span>
+                <h2>". $corso['titolo'] . "</h2>
+                <p>". $corso['descrizione'] . "</p>
+            </div>
         </div>";
     }
 } else {
@@ -27,6 +45,7 @@ $footer = file_get_contents("html/components/footer.html");
 // tag substitutions
 $htmlPage = str_replace("<pageFooter/>", $footer, $htmlPage);
 $htmlPage = str_replace("<elencoCorsi/>", $htmlCorsi, $htmlPage);
+$htmlPage = str_replace("<modalCorsi/>", $modalCorsi, $htmlPage);
 
 echo $htmlPage;
 
