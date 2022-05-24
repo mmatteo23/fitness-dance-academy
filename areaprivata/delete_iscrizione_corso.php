4294895499afaa39d8068cfee7653865fdfc78fb
Matteo Midena <matteo.midena@protonmail.com>
2022-05-24 09:52:15 +0000

    Questi non li aveva caricati

---
+++ areaprivata/delete_iscrizione_corso.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once "../config.php";
+
+require_once(SITE_ROOT . '/php/validSession.php');
+require_once(SITE_ROOT . "/php/Models/Corso.php");
+
+// Get post params
+$id_corso = $_POST['id_corso'];
+
+$modello = new Corso;
+$corso = $modello->read($id_corso);
+
+if($corso !== NULL){
+    if($modello->unregisterUser($id_corso, $_SESSION['userId']))
+        header('Location: prenotazione_corso.php');
+}
+
+// Lo facciamo ritornare e basta? O errore/successo?
+header('Location: prenotazione_corso.php');
+
+?>
---
+++ areaprivata/insert_iscrizione_corso.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once "../config.php";
+
+require_once(SITE_ROOT . '/php/validSession.php');
+require_once(SITE_ROOT . "/php/Models/Corso.php");
+
+// Get post params
+$id_corso = $_POST['id_corso'];
+
+$modello = new Corso;
+$corso = $modello->read($id_corso);
+
+if($corso !== NULL){
+    if($modello->registerUser($id_corso, $_SESSION['userId']))
+        header('Location: prenotazione_corso.php');
+
+} else {
+    echo "ERRORE";
+}
+
+
+?>
