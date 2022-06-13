101052937d40df35efcc0d8784c5a2c6807cf95a
Matteo Midena <matteo.midena@protonmail.com>
2022-06-13 10:17:16 +0000

    Init creazione scheda + Model Esercizio

--- areaprivata/creazione_scheda.php
+++ areaprivata/creazione_scheda.php
@@ -3,88 +3,47 @@
 require_once '../config.php';
 
 require_once(SITE_ROOT . '/php/validSession.php');
-require_once(SITE_ROOT . '/php/Models/Corso.php');
 require_once(SITE_ROOT . '/php/Models/Utente.php');
+require_once(SITE_ROOT . '/php/Models/Esercizio.php');
 
 $errors = '';
 
-$modelloCorso = new Corso();
 $modelloUtente = new Utente();
+$modelloEsercizio = new Esercizio();
+
+$html_table = "
+    <table class='table-prenotazione'>
+        <thead>
+            <tr>
+                <th scope='col'>Esercizio</th>
+                <th scope='col'>Numero Serie</th>
+                <th scope='col'>Ripetizioni</th>
+                <th scope='col'>Pausa</th>
+            </tr>
+        </thead>
+        <tbody>";
+$html_table_footer = "</tbody></table>";
 
 if($_SERVER['REQUEST_METHOD'] == 'POST') {     // Pulsante submit premuto
-    $errors = $modelloCorso->validator($_POST);
-    if($errors === TRUE){
-        $_POST['copertina'] = "default.jpg";
-        $_POST['alt_copertina'] = "Immagine del corso di default";
-        if(!Corso::create($_POST)){
-            $errors = "<p>Qualcosa è andato storto, ci scusiamo per il disagio</p>";
-        }
-        else{
-            header("location: gestione_corso.php");
-        }
-    }
+    
 }
-
-// TAKE OLD USER INFO
-$userData = $modelloUtente->read($_SESSION['userId']);
-
-$formContent = "
-    <div class='input-wrapper'>
-        <label for='titolo'>Titolo*</label>
-        <input type='text' name='titolo' id='titolo' class='transparent-login' onblur='validaTitolo()'>
-        <p class='error'></p>
-    </div>
-    <div class='input-wrapper'>
-        <label for='descrizione'>Descrizione*</label>
-        <input type='text' name='descrizione' id='descrizione' class='transparent-login' onblur='validaDescrizione()'>
-        <p class='error'></p>
-    </div>
-    <div class='input-wrapper'>
-        <label for='data_inizio'>Data di inizio*</label>
-        <input type='date' name='data_inizio' id='data_inizio' class='transparent-login' value='2000-01-01'>
-        <p class='error'></p>
-    </div>
-    <div class='input-wrapper'>
-        <label for='data_fine'>Data di fine*</label>
-        <input type='date' name='data_fine' id='data_fine' class='transparent-login' value='2000-01-01' onblur='validaDate()'>
-        <p class='error'></p>
-    </div>
-    <div class='input-wrapper'>
-        <label for='copertina'>Copertina del corso</label>
-        <input type='file' name='copertina' id='copertina' class='transparent-login' accept='image/png, image/jpeg' onblur='validaImg()'>       
-        <p class='error'></p>
-    </div>
-    <div class='input-wrapper'>
-        <label for='alt_copertina'>Descrizione copertina* (se scelta)</label>
-        <input type='text' name='alt_copertina' id='alt_copertina' class='transparent-login' onblur='validaAltImmagine()'>
-        <p class='error'></p>
-    </div>
-    <div class='input-wrapper'>
-        <label for='trainer'>Trainer*</label>
-        <select name='trainer' id='trainer' class='transparent-login'>
-            <trainerOptions/>
-        </select>
-        <p class='error'></p>
-    </div>
-";
 
 $trainers = $modelloUtente->getTrainers();
+$esercizi = $modelloEsercizio->index();
 
-if(isset($trainers)){
-    $trainerOptions = '';
-    foreach ($trainers as $trainer) {
-        if($trainer['id']==0)
-            $trainerOptions .= "<option value='" . $trainer['id'] . "' selected>" . $trainer['nome'] . " " . $trainer['cognome'] . "</option>";
-        else
-            $trainerOptions .= "<option value='" . $trainer['id'] . "'>" . $trainer['nome'] . " " . $trainer['cognome'] . "</option>";
-    }
+// crea options per selectbox degli esercizi
+$optionsEsercizi = "";
+foreach($esercizi as $esercizio){
+    $optionsEsercizi .= "<option value=". $esercizio["id"] . ">" . $esercizio["nome"] . "</option>";
 }
+
+
 
 $htmlPage = file_get_contents(SITE_ROOT . '/html/areaprivata/creazione_scheda.html');
 $footer = file_get_contents(SITE_ROOT . '/html/components/footer.html');
 
-//$htmlPage = str_replace('<formContent/>', $formContent, $htmlPage);
-$htmlPage = str_replace('<trainerOptions/>', $trainerOptions, $htmlPage);
+$htmlPage = str_replace('<optionsEsercizi/>', $optionsEsercizi, $htmlPage);
+$htmlPage = str_replace('<tableEserciziScheda/>', $html_table.$html_table_footer, $htmlPage);
 $htmlPage = str_replace('<formErrors/>', $errors, $htmlPage);
 $htmlPage = str_replace('<pageFooter/>', $footer, $htmlPage);
 
---
+++ php/Models/Esercizio.php
@@ -0,0 +1,32 @@
+<?php
+require_once(SITE_ROOT . '/php/db.php');
+require_once(SITE_ROOT . '/php/utilities.php');
+use DB\DBAccess;
+
+class Esercizio {
+
+    protected $filtrable_fields = array("nome", "categoria");
+
+    public function index(array $filters = [])
+    {
+        $connection_manager = new DBAccess();
+        $conn_ok = $connection_manager->openDBConnection();
+
+        if($conn_ok){
+            $query = "SELECT * FROM esercizio";
+            // append if there are some filters
+            if(count($filters)) $query .= append_filters($filters, $this->filtrable_fields);
+
+            //echo $query;
+            $queryResults = $connection_manager->executeQuery($query);
+            $connection_manager->closeDBConnection();
+
+            return isset($queryResults)?$queryResults:NULL;
+        }
+
+        return NULL;
+    }
+
+}
+
+?>
